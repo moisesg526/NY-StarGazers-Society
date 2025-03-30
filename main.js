0fb5c4ee8fd725989503a6e37e27b274f8718d6b
Moises Gomez <76762359+moisesg526@users.noreply.github.com>
2025-03-30 17:22:20 -0400

    Got the pod button to retrieve the data from the api

--- main.js
+++ main.js
@@ -1,15 +1,22 @@
-let podBtn = document.querySelector(".pic-Of-Day");
-podBtn.addEventListener("click", getPod);
 let h2 = document.querySelector("h2");
+let p = document.querySelector("p");
+let podBtn = document.querySelector(".pod-btn");
 let img = document.querySelector("img");
 
+podBtn.addEventListener("click", getPod);
+
 function getPod() {
+  let date = document.querySelector(".get-Pod").value;
+
   fetch(
-    "https://api.nasa.gov/planetary/apod?api_key=PhnfsrWFDYqeVJcPZ5KAA0iNzEsWbnilLHuiME4N"
+    `https://api.nasa.gov/planetary/apod?api_key=PhnfsrWFDYqeVJcPZ5KAA0iNzEsWbnilLHuiME4N&date=${date}`
   )
     .then((res) => res.json())
     .then((data) => {
       h2.textContent = data.title;
       img.src = data.hdurl;
-    });
+      img.alt = data.title;
+      p.textContent = data.explanation;
+    })
+    .catch((error) => console.error("Error fetching data:", error));
 }
